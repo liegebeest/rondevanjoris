7a676e6571b4394fdb5bf649136e871c1549f3b4
Joris Custers <joris@amplidata.com>
2017-06-23 22:53:33 +0200

    Add files via upload

---
+++ db.php
@@ -0,0 +1,7 @@
+<?php
+$con = mysqli_connect("rondevanjoris.be.mysql","rondevanjoris_be","rondevanjoris","rondevanjoris_be");
+if (mysqli_connect_errno()) {
+  echo "Failed to connect to MySQL: " . mysqli_connect_error();
+}
+
+?>
