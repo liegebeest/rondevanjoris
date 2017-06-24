a1501b642fdb5b43c948a94e552639b3f04345fb
Joris Custers <joris@amplidata.com>
2017-06-24 15:40:57 +0200

    adding dutch date to routes

--- routes.php
+++ routes.php
@@ -1,26 +1,28 @@
 <?php 
-	require 'db.php';
- 		
- 		$sqltran = mysqli_query($con, "SELECT date, route, price, places, subscribe FROM routes where routes.`date` > now() ORDER BY routes.`date` ASC")or die(mysqli_error($con));
-		$arrVal = array();
- 		$i=1;
- 		while ($rowList = mysqli_fetch_array($sqltran)) {
- 								 
-						$name = array(
- 	 		 	 				'date'=> $rowList['date'],
-	 		 	 				'route'=> $rowList['route'],
-								'price'=> $rowList['price'],
-								'places'=> $rowList['places'],
-								'subscribe'=> $rowList['subscribe']
- 	 		 	 			);		
 
+require 'db.php';
+$sqltran = mysqli_query($con, "SELECT date, route, price, places, subscribe FROM routes where routes.`date` > now() ORDER BY routes.`date` ASC")or die(mysqli_error($con));
+$arrVal = array();
+$i=1;
+while ($rowList = mysqli_fetch_array($sqltran)) 
+{
+    $time=strtotime( $rowList['date']);
+    setlocale (LC_TIME, "nl_NL"); 
+    $dutch_date = strftime("%A %e %B %Y", $time);
+  
+	$name = array(
+			'date'=> $dutch_date,
+   			'route'=> $rowList['route'],
+			'price'=> $rowList['price'],
+			'places'=> $rowList['places'],
+			'subscribe'=> $rowList['subscribe']
+			);		
 
-							array_push($arrVal, $name);	
-			$i++;			
-	 	}
-	 		 echo  json_encode($arrVal);		
+	array_push($arrVal, $name);	
+	$i++;			
+ }
  
+echo  json_encode($arrVal);		
+mysqli_close($con);
 
-	 	mysqli_close($con);
 ?>   
- 
