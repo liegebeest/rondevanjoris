84b8f9f4170181c8e41d69eb5ef8d39d6b356052
Joris Custers <joris@amplidata.com>
2017-08-01 22:05:18 +0200

    route dd

---
+++ js/routedropdown.js
@@ -0,0 +1,10 @@
+$.getJSON("http://rondevanjoris.com/routes.php", function(json){
+        $('#routedropdown').empty();
+        $('#routedropdown').append($('<option>').text("Selecteer een route").attr('value', ''));
+        $.each(json, function(i, obj){
+                $name = obj.date + " " + obj.route + " ("+ obj.places + ")"
+                if (obj.places > 1){
+                    $('#routedropdown').append($('<option>').text($name).attr('value', obj.id));
+                }
+        });
+});
