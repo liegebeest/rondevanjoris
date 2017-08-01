ba9e0ea2f00984c97aa78136e2d26f5ed189d7ea
Joris Custers <joris@amplidata.com>
2017-08-01 23:57:34 +0200

    bugs on drugs

--- js/routedropdown.js
+++ js/routedropdown.js
@@ -3,7 +3,7 @@ $.getJSON("http://rondevanjoris.com/routes.php", function(json){
         $('#routedropdown').append($('<option>').text("Selecteer een route").attr('value', ''));
         $.each(json, function(i, obj){
                 $name = obj.date + " " + obj.route + " ("+ obj.places + ")"
-                if (obj.places > 1){
+                if (obj.places > 0){
                     $('#routedropdown').append($('<option>').text($name).attr('value', obj.id));
                 }
         });
