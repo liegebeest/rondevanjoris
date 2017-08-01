b8ce20527caa253e9a3b78fe681efe0dcc740ca0
Joris Custers <joris@amplidata.com>
2017-08-01 23:15:05 +0200

    add source

--- columns.php
+++ columns.php
@@ -1,13 +1,14 @@
 <?php 
 	require 'db.php';
- 	$sqltran = mysqli_query($con, "SELECT link, name FROM stukjes") or die(mysqli_error($con));
+ 	$sqltran = mysqli_query($con, "SELECT link, name, source FROM stukjes") or die(mysqli_error($con));
 	$arrVal = array();
  	$i=1;
  	while ($rowList = mysqli_fetch_array($sqltran)) {
  							 
 		$column = array(
 		    'link'=> $rowList['link'],
-			'name'=> $rowList['name']
+			'name'=> $rowList['name'],
+			'source'=> $rowList['source']
     		);		
 		array_push($arrVal, $column);	
 		$i++;			
