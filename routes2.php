7acb323a37cd12619f95ff97472bf5bfdce722b5
Joris Custers <joris@amplidata.com>
2017-08-02 21:46:23 +0200

    r2

---
+++ routes2.php
@@ -0,0 +1,31 @@
+<?php 
+
+require 'db.php';
+$sqltran = mysqli_query($con, "SELECT date, route, price, places, id, fotolink FROM routes where routes.`date` < now() and fotolink is not NULL ORDER BY routes.`date` ASC")or die(mysqli_error($con));
+$arrVal = array();
+$i=1;
+
+while ($rowList = mysqli_fetch_array($sqltran)) 
+{
+    $time=strtotime( $rowList['date']);
+    setlocale (LC_TIME, "nl_NL"); 
+    $dutch_date = strftime("%A %e %B %Y", $time);
+  
+    $name = array(
+	    'date'=> $dutch_date,
+   		'route'=> $rowList['route'],
+   		'id' => $rowList['id'],
+		'price'=> $rowList['price'],
+		'places'=> $rowList['places'],
+		'fotolink' => $rowList['fotolink']
+		);		
+
+	array_push($arrVal, $name);	
+	$i++;			
+}
+ 
+echo  json_encode($arrVal);		
+mysqli_close($con);
+
+?>   
+ 
